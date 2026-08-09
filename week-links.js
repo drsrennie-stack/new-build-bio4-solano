9d0d6b1ad5b8a924047d05597b6b718bc64e669c
drsrennie-stack <dr.srennie@gmail.com>
2026-08-08 19:07:03 -0700

    Add files via upload

--- week-links.js
+++ week-links.js
@@ -18,18 +18,18 @@ window.BIO004_WEEK_LINKS = {
    1: { worksheet:"m1-intro-worksheet.html", lecture:"week1-concept-videos.html" }, /* Intro to Anatomy, cells, histology */
    2: { worksheet:"m1-cell-worksheet.html", lecture:"cell-concept-videos.html" }, /* TBL 1 Cell Anatomy, connective tissue */
    3: { worksheet:"m1-tissues-worksheet.html", lecture:"tissues-concept-videos.html" }, /* TBL 2 Histology, integumentary */
-   4: { worksheet:"BIO004-Midterm-Practice-Exams.html", lecture:"tissues-concept-videos.html" }, /* Exam 1 week: practice exams + rewatch */
+   4: { worksheet:"BIO004-Midterm-Practice-Exams.html", lecture:"tissues-concept-videos.html" }, /* Exam 1 week */
    5: { worksheet:"m2-bone-worksheet.html", lecture:"bone-concept-videos.html" }, /* Bone tissue, skull */
    6: { worksheet:"m2-axial-worksheet.html", lecture:"axial-concept-videos.html" }, /* Spine, upper and lower extremity, TBL 3 */
    7: { worksheet:"m2-appendicular-worksheet.html", lecture:"appendicular-concept-videos.html" }, /* Exam 2 */
    8: { worksheet:"m3-heart-worksheet.html", lecture:"heart-concept-videos.html" }, /* Heart, muscle microanatomy */
    9: { worksheet:"m3-heart-worksheet.html", lecture:"heart-concept-videos.html" }, /* TBL 4 Heart, blood */
   10: { worksheet:"m3-vessels-blood-worksheet.html", lecture:"blood-concept-videos.html" }, /* TBL 5 Blood, Exam 3 */
-  11: { worksheet:"m3-respiratory-worksheet.html", lecture:"respiratory-concept-videos.html" }, /* Respiratory, endocrine */
-  12: { worksheet:"m3-respiratory-worksheet.html", lecture:"respiratory-concept-videos.html" }, /* TBL 6 Respiratory, GI map */
-  13: { worksheet:"accessory-digestive-organs-workbook.html", lecture:"accessory-digestive-concept-videos.html" }, /* TBL 7 GI System */
-  14: { worksheet:"urinary-system-workbook.html", lecture:"urinary-concept-videos.html" }, /* Exam 4, renal */
-  15: { worksheet:"reproductive-female-workbook.html", lecture:"reproductive-concept-videos.html" }, /* Renal, reproductive */
-  16: { worksheet:"urinary-system-workbook.html", lecture:"urinary-concept-videos.html" }, /* TBL 8 Renal, CNS */
-  17: { worksheet:"cranial-nerves-workbook.html", lecture:"cranial-nerves-concept-videos.html" }, /* TBL 9 Cranial nerves, Exam 5 */
+  11: { worksheet:"m4-resp-worksheet.html", lecture:"respiratory-concept-videos.html" }, /* Respiratory, endocrine */
+  12: { worksheet:"m4-resp-worksheet.html", lecture:"respiratory-concept-videos.html" }, /* TBL 6 Respiratory, GI map */
+  13: { worksheet:"m4-digest-worksheet.html", lecture:"accessory-digestive-concept-videos.html" }, /* TBL 7 GI System */
+  14: { worksheet:"m4-urinary-worksheet.html", lecture:"urinary-concept-videos.html" }, /* Exam 4, renal */
+  15: { worksheet:"m4-repro-worksheet.html", lecture:"reproductive-concept-videos.html" }, /* Renal, reproductive */
+  16: { worksheet:"m5-cord-worksheet.html", lecture:"urinary-concept-videos.html" }, /* TBL 8 Renal, CNS */
+  17: { worksheet:"m5-pns-worksheet.html", lecture:"cranial-nerves-concept-videos.html" }, /* TBL 9 Cranial nerves, Exam 5 */
 };
